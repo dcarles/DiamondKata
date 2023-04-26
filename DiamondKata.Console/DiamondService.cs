9d7126312dc1bfc79abb0b6fbc37d140b3229618
Daniel Carles <danielcarles@gmail.com>
2023-04-26 22:01:38 +0100

    RenderRow with tests and generate diamond " A /nB B/n A " using row method

--- DiamondKata.Console/DiamondService.cs
+++ DiamondKata.Console/DiamondService.cs
@@ -1,15 +1,48 @@
-﻿namespace DiamondKata.Console;
+﻿using System.Text;
+
+namespace DiamondKata.Console;
 
 public class DiamondService
 {
     public string RenderDiamond(char c)
     {
         if (c == 'A')
-            return "A";
+            return RenderRow(1, 0);
         else
-            return " A " + Environment.NewLine +
-                   "B B" + Environment.NewLine +
-                   " A " + Environment.NewLine;
+            return RenderRow(2, 0) + Environment.NewLine +
+                   RenderRow(2, 1) + Environment.NewLine +
+                   RenderRow(2, 0) + Environment.NewLine;
+    }
+
+    public string RenderRow(int numberOfRows, int rowIndex)
+    {
+        var row = new StringBuilder();
+
+        //We can use the numeric value of char to calculate next char in alphabetical order
+        //then convert back to char
+        var currentChar = (char)('A' + rowIndex);
+
+        //External padding is calculated as per Readme analysis
+        var externalPadding = (numberOfRows - rowIndex - 1);
+
+        //left padding
+        row.Append(new string(' ', externalPadding));
+
+        row.Append(currentChar);
+
+        //A is the only single char so dont need
+        if (currentChar != 'A')
+        {
+            //Internal padding is calculated as per Readme analysis
+            var internalPadding = (rowIndex * 2) - 1;
+            row.Append(new string(' ', internalPadding));
+
+            row.Append(currentChar);
+        }
+
+        //right padding
+        row.Append(new string(' ', externalPadding));
+        return row.ToString();
     }
 }
 
--- DiamondKata.Tests.Unit/DiamondServiceTests.cs
+++ DiamondKata.Tests.Unit/DiamondServiceTests.cs
@@ -17,19 +17,60 @@ public class DiamondServiceTests
     [Fact]
     public void RenderDiamond_WhenA_ReturnA()
     {
+        // Arrange
+        var expected = "A";
+
+        //Act
         var result = _diamondService.RenderDiamond('A');
-        result.Should().Be("A");
+
+        //Assert
+        result.Should().Be(expected);
     }
 
     [Fact]
     public void RenderDiamond_WhenB_ReturnABDiamond()
     {
+        // Arrange
         var expectedDiamond = new StringBuilder();
         expectedDiamond.AppendLine(" A ");
         expectedDiamond.AppendLine("B B");    
         expectedDiamond.AppendLine(" A ");
 
+        //Act
         var result = _diamondService.RenderDiamond('B');
+
+        //Assert
         result.Should().Be(expectedDiamond.ToString());
+    }
+
+    [Fact]
+    public void RenderRow_WithMinimumValues_ReturnsA()
+    {
+        // Arrange
+        int numberOfRows = 1;
+        int rowIndex = 0;
+        var expectedRow = "A";
+
+        // Act
+        var actualRow = _diamondService.RenderRow(numberOfRows, rowIndex);
+
+        // Assert
+        actualRow.Should().Be(expectedRow);
+       
+    }
+
+    [Fact]
+    public void RenderRow_With2RowsIndex1_ReturnsBBRowWithCorrectInternalPadding()
+    {
+        // Arrange
+        int numberOfRows = 2;
+        int rowIndex = 1;
+        var expectedRow = "B B";
+
+        // Act
+        var actualRow = _diamondService.RenderRow(numberOfRows, rowIndex);
+
+        // Assert
+        Assert.Equal(expectedRow, actualRow);
     }
 }
